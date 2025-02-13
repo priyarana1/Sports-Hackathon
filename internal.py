b87e4c1a35276351269baee5b0b014610bcf756d
Priya Rana <121190961+priyarana1@users.noreply.github.com>
2025-02-13 17:08:50 -0500

    Create internal.py

---
+++ internal.py
@@ -0,0 +1,27 @@
+import requests
+
+#checks how many requests, ensures we aren't above daily limit
+
+# API endpoint
+url = "https://v1.basketball.api-sports.io/status"
+
+apiKey = "b12e1a56d3f6803de2e8961389f6768f" #api key (priya's profile)
+
+headers = {
+    "x-apisports-key": apiKey 
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print("API Response:", data)
+
+    if "response" in data and isinstance(data["response"], dict):
+        current_requests = data["response"]["requests"]["current"]
+        daily_limit = data["response"]["requests"]["limit_day"]
+        print(f"Current API requests used: {current_requests}/{daily_limit}")
+    else:
+        print("Error: No valid data in 'response'. Check your API key and request.")
+else:
+    print(f"Error {response.status_code}: {response.text}")
