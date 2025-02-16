32a7ee28dbb6eaffea8119a83966c5be5326f93f
Krish <krishm.imp@gmail.com>
2025-02-16 00:31:11 -0500

    leaderboard

--- rutgers-basketball-dashboard/src/App.js
+++ rutgers-basketball-dashboard/src/App.js
@@ -1,4 +1,9 @@
 import logo from './logo.svg';
+import React from "react"
+import Leaderboard from "./components/Leaderboard";
+import Voting from "./components/Votingapp";
+import Stats from "./components/Stats";
+import Rewards from "./components/Rewards";
 import './App.css';
 
 function App() {
@@ -6,9 +11,6 @@ function App() {
     <div Classname = "App">
       <h1> Rutger Basketball Fan Dashboard </h1>
       <Leaderboard/>
-      <Voting/>
-      <Stats/>
-      <Rewards/>
     </div>
   )
 }
